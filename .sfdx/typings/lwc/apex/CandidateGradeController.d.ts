4b72f99d93b360aaae44d6f18b115358eeb9ab0f
Lorenzo Frattini <lorenzo@clayton.io>
2021-07-22 16:27:13 +0200

    minor

--- .sfdx/typings/lwc/apex/CandidateGradeController.d.ts
+++ .sfdx/typings/lwc/apex/CandidateGradeController.d.ts
@@ -1,3 +1,6 @@
 declare module "@salesforce/apex/CandidateGradeController.getCandidateGrade" {
   export default function getCandidateGrade(param: {contactId: any}): Promise<any>;
 }
+declare module "@salesforce/apex/CandidateGradeController.getCandidateGrade2" {
+  export default function getCandidateGrade2(param: {contactId: any}): Promise<any>;
+}
---
+++ .sfdx/typings/lwc/apex/LookupController2.d.ts
@@ -0,0 +1,3 @@
+declare module "@salesforce/apex/LookupController2.findRecords" {
+  export default function findRecords(param: {searchField: any, searchValue: any, objectName: any, parentField: any, parentValue: any, orderByField: any, orderDirection: any}): Promise<any>;
+}
