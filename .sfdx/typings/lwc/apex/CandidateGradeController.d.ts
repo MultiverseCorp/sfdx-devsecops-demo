6e99cff27a80ef927364aa4aca3abf6e42999898
Lorenzo Frattini <lorenzo@clayton.io>
2021-07-22 16:28:23 +0200

    improvement: levels

--- .sfdx/typings/lwc/apex/CandidateGradeController.d.ts
+++ .sfdx/typings/lwc/apex/CandidateGradeController.d.ts
@@ -1,6 +1,3 @@
 declare module "@salesforce/apex/CandidateGradeController.getCandidateGrade" {
   export default function getCandidateGrade(param: {contactId: any}): Promise<any>;
 }
-declare module "@salesforce/apex/CandidateGradeController.getCandidateGrade2" {
-  export default function getCandidateGrade2(param: {contactId: any}): Promise<any>;
-}
