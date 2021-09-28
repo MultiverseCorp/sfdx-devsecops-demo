5326b25fad4f2189f061bd1b19b9d4ed70ac4a9c
gianc <giancarlolallopizzi@gmail.com>
2021-09-28 10:01:29 +0200

    latest commit for test

---
+++ .sfdx/typings/lwc/apex/CandidateGradeController4%20copy.d.ts
@@ -0,0 +1,6 @@
+declare module "@salesforce/apex/CandidateGradeController4.getCandidateGrade4" {
+  export default function getCandidateGrade4(param: {contactId: any}): Promise<any>;
+}
+declare module "@salesforce/apex/CandidateGradeController4.getCandidateGrade5" {
+  export default function getCandidateGrade5(param: {contactId: any}): Promise<any>;
+}
